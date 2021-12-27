0878361b9cce1a8dbbfd1344d0268d32e2973f4f
Madhukar Mani <madhumani1@gmail.com>
2021-12-27 11:34:11 -0500

    get customers and get customers/{customerID}

---
+++ src/main/java/com/madhu/demo/rest/CustomerRestController.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.madhu.demo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.madhu.demo.entity.Customer;
+import com.madhu.demo.service.CustomerService;
+
+/**
+ * @author 15197
+ *
+ */
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	// autowire the CustomerService
+	@Autowired
+	private CustomerService customerService;
+	
+	// add mapping for GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers()	{
+		return customerService.getCustomers();
+	}
+	
+	// add mapping for GET /customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId)	{
+		Customer theCustomer = customerService.getCustomer(customerId);
+		return theCustomer;
+	}
+}
